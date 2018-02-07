c14363671501eeb643571c4f5c2a18b3a94fa7b6
babadav <babadav@gmail.com>
2018-02-06 19:37:47 -0500

    CHris check this out

--- src/js/app.js
+++ src/js/app.js
@@ -5,29 +5,60 @@
 
 TweenMax.to('.plant-glow', 3, {
 	opacity: 1,
-	repeat: 10,
+	repeat: -1,
 	yoyo: true
 
 })
 
-var gridWidth = 200;
+TweenMax.to('.small-pink-glow', 2, {
+     opacity: 1,
+     repeat: -1,
+     yoyo: true
+
+})
+
+TweenMax.to('.large-pink-glow', 4, {
+     opacity: 1,
+     repeat: 10,
+     yoyo: true
+
+})
+
+TweenMax.to('.face-dots-1', 4, {
+     opacity: 1,
+     repeat: -1,
+     yoyo: true
+
+})
+
+TweenMax.to('.face-dots-2', 2, {
+     opacity: 1,
+     repeat: -1,
+     yoyo: true
+
+})
+
+
+
+
+var gridWidth = window.innerWidth;
 var gridHeight = 100;
 Draggable.create("body", {
 
-    type:"x,y",
+    type:"x", //"x,y",
     edgeResistance:0.65,
     bounds:"",
     lockAxis:true,
-    liveSnap: true,
+    liveSnap: false,
     throwProps:true,
     snap: {
         x: function(endValue) {
             return Math.round(endValue / gridWidth) * gridWidth;
 
         },
-        y: function(endValue) {
-            return Math.round(endValue / gridHeight) * gridHeight;
-        }
+        // y: function(endValue) {
+        //     return Math.round(endValue / gridHeight) * gridHeight;
+        // }
     }
 
 });
