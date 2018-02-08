29668e92c424ff4e3bba39b3a45ecb25639256ab
babadav <babadav@gmail.com>
2018-02-08 18:55:15 -0500

    turning in

--- src/js/app.js
+++ src/js/app.js
@@ -1,64 +1,88 @@
 
-(() => {
-	console.log("Hello World from app.js, transpiled and concatenated!");
-})();
+// (() => {
+// 	console.log("Hello World from app.js, transpiled and concatenated!");
+// })();
 
-TweenMax.to('.plant-glow', 3, {
-	opacity: 1,
-	repeat: -1,
-	yoyo: true
+// TweenMax.to('.plant-glow', 3, {
+// 	opacity: 1,
+// 	repeat: -1,
+// 	yoyo: true
 
-})
+// })
 
-TweenMax.to('.small-pink-glow', 2, {
-     opacity: 1,
-     repeat: -1,
-     yoyo: true
+// TweenMax.to('.small-pink-glow', 2, {
+//      opacity: 1,
+//      repeat: -1,
+//      yoyo: true
 
-})
+// })
 
-TweenMax.to('.large-pink-glow', 4, {
-     opacity: 1,
-     repeat: 10,
-     yoyo: true
+// TweenMax.to('.large-pink-glow', 4, {
+//      opacity: 1,
+//      repeat: 10,
+//      yoyo: true
 
-})
+// })
 
-TweenMax.to('.face-dots-1', 4, {
-     opacity: 1,
-     repeat: -1,
-     yoyo: true
+// TweenMax.to('.face-dots-1', 4, {
+//      opacity: 1,
+//      repeat: -1,
+//      yoyo: true
 
-})
+// })
 
-TweenMax.to('.face-dots-2', 2, {
-     opacity: 1,
-     repeat: -1,
-     yoyo: true
+// TweenMax.to('.face-dots-2', 2, {
+//      opacity: 1,
+//      repeat: -1,
+//      yoyo: true
 
-})
-
-
+// })
 
 
-var gridWidth = window.innerWidth;
-var gridHeight = 100;
-Draggable.create("body", {
 
-    type:"x", //"x,y",
-    edgeResistance:0.65,
-    bounds:"",
-    lockAxis:true,
-    liveSnap: false,
-    throwProps:true,
-    snap: {
-        x: function(endValue) {
-            return Math.round(endValue / gridWidth) * gridWidth;
 
-        },
-        // y: function(endValue) {
-        //     return Math.round(endValue / gridHeight) * gridHeight;
-        // }
-    }
+// var gridWidth = window.innerWidth;
 
-});
+// // Draggable.create("body", {
+
+// //     type:"x", //"x,y",
+// //     edgeResistance:0.65,
+// //     bounds:"",
+// //     lockAxis:true,
+// //     liveSnap: false,
+// //     throwProps:true,
+// //     snap: {
+// //         x: function(endValue) {
+// //             return Math.round(endValue / gridWidth) * gridWidth;
+
+// //         },
+// //         // y: function(endValue) {
+// //         //     return Math.round(endValue / gridHeight) * gridHeight;
+// //         // }
+// //     }
+
+// // });
+
+
+// var animalsContainer = document.querySelector('.animals-container');
+
+// var hammertime = new Hammer(animalsContainer);
+// hammertime.on('swipeleft', function(ev) {
+//     console.log(ev);
+//     alert("swipeleft!")
+// });
+
+// hammertime.on('panleft', function(ev) {
+//     console.log(ev);
+//     alert("panleft!")
+// });
+//     console.log(hammertime)
+
+// // var swipe  = new Hammer.Swipe();
+// // hammertime.add(swipe);
+
+// // hammertime.on('pan', function(){
+// //     console.log(this)
+// // })
+
+
